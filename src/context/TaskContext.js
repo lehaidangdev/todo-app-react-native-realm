28ec1873bc3f874378d08a972473ec6787d0bef2
LeHaiDang <lehaidangdev@gmail.com>
2021-07-04 13:04:41 +0700

    add new task feature

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,8 +1,14 @@
-import React, {useState, createContext} from 'react';
+import React, {useState, createContext, useEffect} from 'react';
 import Realm from 'realm';
 
 //** REALM SECTION */
+//* STEP*/
+// 1. define object model aka schema
+// 2. open connection
+// 3. CRUD aka 'do something' with data
+// 4. close connection
 
+//* STEP 1  - Define Object model */
 const taskSchema = {
   name: 'Task',
   properties: {
@@ -24,7 +30,15 @@ const taskList = [
 
 const TaskContextProvider = ({children}) => {
   const [tasks, setTasks] = useState(taskList);
-  const addTask = task => {};
+  const addTask = task => {
+    let newTask = {
+      id: 4,
+      name: task,
+      status: 'doing',
+    };
+    let newTasks = [...tasks, newTask];
+    setTasks(newTasks);
+  };
 
   const removeTask = task => {
     let remained = tasks.filter(item => item !== task);
--- src/screens/AddTaskScreen.js
+++ src/screens/AddTaskScreen.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import {TaskContext} from '../context/TaskContext';
+import AppUtils from '../utils/AppUtils';
 
 const AddTaskScreen = props => {
-  return <SafeAreaView style={styles.screen}></SafeAreaView>;
+  const {addTask} = useContext(TaskContext);
+  const [inputText, setInputText] = useState('');
+
+  const addBtn = (
+    <TouchableOpacity style={styles.addBtn} onPress={() => addHandler()}>
+      <Text style={{color: '#FFF', fontSize: 18}}>Add</Text>
+    </TouchableOpacity>
+  );
+
+  const addHandler = () => {
+    addTask(inputText);
+    props.navigation.goBack();
+  };
+
+  const taskInput = (
+    <TextInput
+      style={styles.input}
+      placeholder="Which you want to do?"
+      onChangeText={text => setInputText(text)}
+    />
+  );
+  return (
+    <SafeAreaView style={styles.screen}>
+      {taskInput}
+      {!AppUtils.isEmpty(inputText) && addBtn}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    margin: 10,
+    backgroundColor: '#e5e5e5',
+    padding: 20,
+    width: '90%',
+  },
+  addBtn: {
+    backgroundColor: '#48cae4',
+    width: '20%',
+    height: '5%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
   },
 });
 
