8e7fab722b4e3127ce05833b360d0b208a0e6c70
LeHaiDang <lehaidangdev@gmail.com>
2021-07-04 08:41:07 +0700

    init commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
